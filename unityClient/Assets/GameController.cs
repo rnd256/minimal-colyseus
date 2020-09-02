42036d96aac2fdd3a0ed9016fc2c862af64e0943
rnd256 <rnd256@users.noreply.github.com>
2020-09-02 18:58:50 -0400

    Wire up `OnError` and `OnLeave`, and add GameController to DefaultScene.

--- unityClient/Assets/GameController.cs
+++ unityClient/Assets/GameController.cs
@@ -6,11 +6,21 @@ public class GameController : MonoBehaviour {
   async void Start() {
     Client client = new Colyseus.Client("ws://localhost:2567");
 
+    Room<State> room;
     try {
-      Room<State> room = await client.JoinOrCreate<State>("demo");
+      room = await client.JoinOrCreate<State>("demo");
       Debug.Log("Joined room successfully!");
     } catch (Exception e) {
       Debug.Log("Error joining: " + e.Message);
+      return;
     }
+
+    room.OnError += (int code, string message) => {
+      Debug.Log(string.Format("Error code {0}. Message: {1}", code, message));
+    };
+
+    room.OnLeave += (NativeWebSocket.WebSocketCloseCode code) => {
+      Debug.Log(string.Format("Leaving room with code: {0}", code));
+    };
   }
 }
