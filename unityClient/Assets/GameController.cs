6197deb5c5dced3b8b0a7066b5d7c9225919db74
rnd256 <rnd256@users.noreply.github.com>
2020-09-02 18:50:58 -0400

    Add GameController.

---
+++ unityClient/Assets/GameController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Colyseus;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+  async void Start() {
+    Client client = new Colyseus.Client("ws://localhost:2567");
+
+    try {
+      Room<State> room = await client.JoinOrCreate<State>("demo");
+      Debug.Log("Joined room successfully!");
+    } catch (Exception e) {
+      Debug.Log("Error joining: " + e.Message);
+    }
+  }
+}
